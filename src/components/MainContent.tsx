45aa622e7aaab29df9ff55a27e3bc5f2105fb6d2
Soila Pertet <nicolepertet@gmail.com>
2025-05-19 20:24:24 -0600

    Add bg colour to team responsibilities

--- src/components/MainContent.tsx
+++ src/components/MainContent.tsx
@@ -5,12 +5,14 @@ const MainContent = () => {
   return (
     <>
       <h1 className='text-4xl p-5 bg-periwinkle'>BMERIT Comms Team</h1>
-      <h3 className='text-3xl font-bold'>Team Responsibilities</h3>
-      <p className="text-lg">
-        The Communications Team manages the club’s social media presence by creating engaging content, 
-        promoting events, and maintaining consistent branding across platforms to increase visibility and 
-        member engagement.
-      </p>
+      <div className='bg-teagreen'>
+        <h3 className='text-3xl font-bold'>Team Responsibilities</h3>
+        <p className="text-lg">
+          The Communications Team manages the club’s social media presence by creating engaging content, 
+          promoting events, and maintaining consistent branding across platforms to increase visibility and 
+          member engagement.
+        </p>
+      </div>
       <div className='bg-teagreen p-5 w-25 text-night flex flex-col justify-center items-center'>
         {
           commsMembers.map((member) => {
